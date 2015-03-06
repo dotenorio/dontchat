7b34bfb1ced5d4157a0d6060d0b47634eb7bfdab
Fernando Migliorini Tenório <dotenorio@gmail.com>
2015-03-06 15:59:15 -0300

    Convertendo as views para Jade para reaproveitamento de código

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ var app = express();
 var http = require('http').Server(app);
 var io = module.exports.io = require('socket.io')(http);
 var crypto = module.exports.crypto = require('crypto');
-require('./src/room');
+var jade = require('jade');
 
 require('./config/globals');
+require('./src/room');
 
 app.use(express.static(VIEWS_PATH + '/assets'));
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
-  res.sendFile(VIEWS_PATH + '/index.html');
+  res.render(VIEWS_PATH + '/index');
 });
 
 app.get('/:password', function(req, res){
-  res.sendFile(VIEWS_PATH + '/chat.html');
+  res.render(VIEWS_PATH + '/chat');
 });
 
 http.listen(3000, function(){
